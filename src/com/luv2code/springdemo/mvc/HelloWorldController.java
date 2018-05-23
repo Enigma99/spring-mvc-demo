f361f39ca13e84c14352474522fc48dd9795a9b5
Enigma James <enigma_1m2@yahoo.com>
2018-05-22 23:35:50 -0400

    add RequestParam example

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -34,6 +35,22 @@ public class HelloWorldController {
 
         // create the message
         String theResult = "Yo, " + name;
+
+        // add message to the model -- name/value pair
+        model.addAttribute("message", theResult);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name,
+                                          Model model){
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String theResult = "Hey My Friend v3! , " + name;
 
         // add message to the model -- name/value pair
         model.addAttribute("message", theResult);
